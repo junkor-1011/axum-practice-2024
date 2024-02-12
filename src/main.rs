42b3ca43508e626c0b5e6bb044c939f6889c1bf4
junkor-1011 <tomoyukizxy@gmail.com>
2024-02-12 15:50:05 +0900

    logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use axum::{routing::get, Router};
+use std::env;
 use std::net::SocketAddr;
+
+const LOG_LEVEL_ENV: &str = "RUST_LOG";
 
 #[tokio::main]
 async fn main() {
+    let log_level = env::var(LOG_LEVEL_ENV).unwrap_or("info".to_string());
+    env::set_var(LOG_LEVEL_ENV, log_level);
+    tracing_subscriber::fmt::init();
+
     let app = Router::new().route("/", get(root));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    tracing::debug!("listening on {addr}");
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
