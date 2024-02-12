6227aa33b3fc164858c546b3d565066933f4a660
junkor-1011 <tomoyukizxy@gmail.com>
2024-02-12 16:13:14 +0900

    logging json(wip)

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ async fn main() {
     let log_level = env::var(LOG_LEVEL_ENV).unwrap_or("info".to_string());
     env::set_var(LOG_LEVEL_ENV, log_level);
     tracing_subscriber::fmt::init();
+    // tracing_subscriber::fmt().json().init();
 
     let app = Router::new().route("/", get(root));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
