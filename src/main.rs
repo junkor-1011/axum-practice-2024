c74c5b4995af0d07208ec581c5c8f07104e1b3bd
junkor-1011 <tomoyukizxy@gmail.com>
2024-02-12 17:00:19 +0900

    json request & response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use axum::{routing::get, Router};
+use axum::{
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use chrono::Utc;
+use hyper::StatusCode;
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::net::SocketAddr;
 
@@ -11,7 +18,10 @@ async fn main() {
     tracing_subscriber::fmt::init();
     // tracing_subscriber::fmt().json().init();
 
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/greet", get(greet))
+        .route("/greet", post(greet_post));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     tracing::debug!("listening on {addr}");
@@ -23,4 +33,42 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, world!"
+}
+
+async fn greet() -> impl IntoResponse {
+    tracing::debug!("greet");
+
+    let body = GreetGetResponse {
+        message: "Hello, World!!!".to_string(),
+        date: Utc::now().to_string(),
+    };
+
+    (StatusCode::OK, Json(body))
+}
+
+async fn greet_post(Json(payload): Json<GreetPostRequest>) -> impl IntoResponse {
+    let GreetPostRequest { name } = payload;
+    let body = GreetPostResponse {
+        message: format!("Hello, {name}"),
+        date: Utc::now().to_string(),
+    };
+
+    (StatusCode::OK, Json(body))
+}
+
+#[derive(Serialize, Debug)]
+struct GreetGetResponse {
+    message: String,
+    date: String,
+}
+
+#[derive(Deserialize)]
+struct GreetPostRequest {
+    name: String,
+}
+
+#[derive(Serialize, Debug)]
+struct GreetPostResponse {
+    message: String,
+    date: String,
 }
