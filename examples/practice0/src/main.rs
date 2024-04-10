e8f831c990ff5a96a005b6e67c978e89159aa6ba
junkor-1011 <tomoyukizxy@gmail.com>
2024-04-11 00:15:35 +0900

    separate files

--- examples/practice0/src/main.rs
+++ examples/practice0/src/main.rs
@@ -1,13 +1,7 @@
-use axum::{
-    response::IntoResponse,
-    routing::{get, post},
-    Json, Router,
-};
-use chrono::Utc;
-use hyper::StatusCode;
-use serde::{Deserialize, Serialize};
 use std::env;
 use std::net::SocketAddr;
+
+use practice0::create_app;
 
 const LOG_LEVEL_ENV: &str = "RUST_LOG";
 
@@ -29,53 +23,4 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
-}
-
-fn create_app() -> Router {
-    Router::new()
-        .route("/", get(root))
-        .route("/greet", get(greet))
-        .route("/greet", post(greet_post))
-}
-
-async fn root() -> &'static str {
-    "Hello, world!"
-}
-
-async fn greet() -> impl IntoResponse {
-    tracing::debug!("greet");
-
-    let body = GreetGetResponse {
-        message: "Hello, World!!!".to_string(),
-        date: Utc::now().to_string(),
-    };
-
-    (StatusCode::OK, Json(body))
-}
-
-async fn greet_post(Json(payload): Json<GreetPostRequest>) -> impl IntoResponse {
-    let GreetPostRequest { name } = payload;
-    let body = GreetPostResponse {
-        message: format!("Hello, {name}"),
-        date: Utc::now().to_string(),
-    };
-
-    (StatusCode::OK, Json(body))
-}
-
-#[derive(Serialize, Debug)]
-struct GreetGetResponse {
-    message: String,
-    date: String,
-}
-
-#[derive(Deserialize)]
-struct GreetPostRequest {
-    name: String,
-}
-
-#[derive(Serialize, Debug)]
-struct GreetPostResponse {
-    message: String,
-    date: String,
 }
