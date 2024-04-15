08b94cb7afe408d67069012d484e3258b718aef9
junkor-1011 <tomoyukizxy@gmail.com>
2024-04-15 21:26:56 +0900

    refactor: html

--- examples/practice1/src/handlers.rs
+++ examples/practice1/src/handlers.rs
@@ -3,5 +3,17 @@ use axum::response::Html;
 pub async fn hello() -> Html<String> {
     let dt = chrono::Utc::now();
 
-    Html(format!("<html><head><title>example</title></head><body><h1>Hello, World!</h1><p>now: {}</p></body></html>", dt))
+    Html(format!(
+        "
+<html>
+  <head>
+    <title>example</title>
+  </head>
+  <body>
+    <h1>Hello, World!</h1>
+    <p>now: {}</p>
+  </body>
+</html>",
+        dt
+    ))
 }
